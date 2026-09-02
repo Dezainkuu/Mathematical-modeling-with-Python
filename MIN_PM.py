2e3f035c92ad658f3c1b547b1f0da36085e66fe4
Mati <maty05112002@gmail.com>
2026-09-02 16:45:27 -0300

    update

--- MIN_PM.py
+++ MIN_PM.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy.integrate as integrate
+
 
 def fun1(x):
     f = np.cos(4*x) * np.cos (3 * np.sin(x))
@@ -9,15 +9,15 @@ def fun1(x):
 #método de integración por punto medio
 def punto_medio (a, b, n):
     h = (b - a)/n  # Ancho de los subintervalos
-    x = np.linspace (a+0.5*h, b-0.5*h, n)
-    y = fun1 (x)
+    x = np.linspace (a+0.5*h, b-0.5*h, n) #n son los puntos, por lo que necesitamos n+1 para los intervalos y el recorrido correcto
+    y = fun1 (x)  # Reemplaza fun1 por la función real
     I_pm = h*np.sum(y)
     return I_pm
 
 #método de integración trapezoidal
 def trapezoidal (a, b, n):
     h = (b - a)/n
-    x = np.linspace (a, b, n+1) #n son los puntos, por lo que necesitamos n+1 para los intervalos y el recorrido correcto
+    x = np.linspace (a, b, n+1) 
     y = fun1 (x)
     S = y[0] + 2*np.sum(y[2:n-1]) + y[n]
     I_trpz = 0.5*h*S
@@ -32,7 +32,7 @@ def simpson13(a, b, n): # El método requiere estrictamente un número par de in
     
     h = (b - a) / n
     x = np.linspace(a, b, n + 1)
-    y = fun1(x) # Reemplaza fun1 por tu función real
+    y = fun1(x)
     
     #suma_extremos = y[0] + y[n]
     #suma_impares = np.sum(y[1:n:2])     # Índices 1, 3, 5... hasta n-1
@@ -45,6 +45,4 @@ def simpson13(a, b, n): # El método requiere estrictamente un número par de in
     return I_simpson
 
 
-print (simpson13(0, np.pi, 100))
-#print (trapezoidal(0, np.pi, 100))
 
